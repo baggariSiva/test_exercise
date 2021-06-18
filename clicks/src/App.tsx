a593e3237869816e988921578ff73fc85bf7f0c6
baggariSiva <narayana@softsuave.com>
2021-06-19 03:54:52 +0530

    Adding css file

--- clicks/src/App.tsx
+++ clicks/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./components/App.css";
+import "./App.css";
 import {inject, IWrappedComponent, observer} from "mobx-react";
 import {CounterStore} from "./counterStore";
 
